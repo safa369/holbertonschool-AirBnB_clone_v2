3972f45b4081c0bfc28af87b96c69837d40c2fb1
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-10 16:58:57 +0100

    setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
