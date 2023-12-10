04d7c49f827bce0021530674dc4f58657f848449
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-10 16:01:52 +0100

    setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- create a data base in my own server.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_db';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES.
