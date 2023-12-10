40ddf6592c034fb8789e61055a84e6ce620aa64f
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-10 16:41:36 +0100

    setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,7 @@
 -- create a data base in my own server.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
-CREATE USER IF NOT EXISTS'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_db';
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
