74e0c4e657e84585fe2b21b26be61f343d431eea
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-21 17:50:51 +0100

    route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""scrit start  a Flask Web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+if __name__ == "__main__":
+    """run application with port 5000 and host 0.0.0.0"""
+    app.run(host='0.0.0.0', port=5000)
