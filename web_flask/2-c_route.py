2f3727f83f31198cacd1a1d86aa53566b897a476
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-22 11:35:20 +0100

    flsak

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/',  strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(name):
+    return f'c {name}'
+
+
+if __name__ == "__main__":
+    """run application with port 5000 and host 0.0.0.0"""
+    app.run(host='0.0.0.0', port=5000)
