ef105fce7710eed6125d3e6cd694b8bb929df8dc
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-26 13:24:09 +0100

    weeb flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,6 +35,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """return a page HTML"""
     return render_template('5-number.html', n=n)
 
 
