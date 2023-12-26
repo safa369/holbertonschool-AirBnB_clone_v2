f67d877c60bc6b54430d82351666f46808abe1c1
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-26 12:57:06 +0100

    web flask

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Python is cool"""
+
+from flask import Flask, render_template
+from templates import number
+app = Flask(__name__)
+
+
+@app.route('/',  strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<name>', strict_slashes=False)
+def c(name):
+    name = name.replace('_', ' ')
+    return 'C {}'.format(name)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def pyth_text(text='is cool'):
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template(number.html, n)
+
+
+if __name__ == "__main__":
+    """run application with port 5000 and host 0.0.0.0"""
+    app.run(host='0.0.0.0', port=5000)
