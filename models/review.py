016978c2ed5f99040b11d422f8b531c0ebbea374
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-29 14:51:29 +0100

    review

---
+++ models/review.py
@@ -0,0 +1,13 @@
+""" a class review"""
+
+from base_model import BaseModel
+
+class Review(BaseModel):
+    """a class of review
+        attr:
+            place_id: string
+            user_id: string
+            text: string"""
+    place_id = ""
+    user_id = ""
+    text = ""
