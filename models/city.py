fa1e4f3afd4b544cd24de224cbd5ba288e76da96
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-29 14:50:05 +0100

    city

---
+++ models/city.py
@@ -0,0 +1,11 @@
+""" a class City"""
+
+from base_model import BaseModel
+
+class City(BaseModel):
+    """ a class city
+        attr:
+            state_id: id of city
+            name: name of city"""
+    name =""
+    state_id = ""
