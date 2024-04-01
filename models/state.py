07a4a8e9381f69be810bcd3bc5b7699601eef821
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-01 14:33:38 +0200

    models

--- models/state.py
+++ models/state.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
 """ a class state"""
 
-from base_model import BaseModel
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import column, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """state
     att: 
         name: string name of state"""
-    
-    name = ""
+    __tablename__ = "states"
+    name = column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
