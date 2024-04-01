351c2dcf70558ede407b1f3039d97b2e10a4946b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-01 14:21:23 +0200

    user

--- models/user.py
+++ models/user.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """ a class user"""
 
-from base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
-class User(BaseModel):
-    """a class user inherits from Basemode
-    attr:
-        email: string email of user.
-        password: password of user.
-        first_name: first name of user
-        last_name: last name of user"""
+class User(BaseModel, Base):
+    """Represents a user."""
+    __tablename__ = 'users'
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
