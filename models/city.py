268ffb22bc9b7c222ae3ce10fa178dbbbedc3a7d
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-04 14:23:47 +0200

    city

--- models/city.py
+++ models/city.py
@@ -8,11 +8,10 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """ The city class, contains state ID & name """
     __tablename__ = "cities"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities",
-                              cascade="all, delete, delete-orphan")
-    else:
-        state_id = ""
-        name = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60),
+                      ForeignKey("states.id", ondelete="CASCADE"),
+                      nullable=False)
+    places = relationship("Place", backref="cities",
+                          cascade="all, delete-orphan",
+                          passive_deletes=True)
