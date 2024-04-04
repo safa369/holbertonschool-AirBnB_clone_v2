3bdb059fd5446135e8b865ab88395c7f596dcd77
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-04 14:36:26 +0200

    place

--- models/place.py
+++ models/place.py
@@ -7,13 +7,31 @@ from os import getenv
 
 
 class Place(BaseModel, Base):
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        metadata = Base.metadata
-        place_amenity = Table("place_amenity", metadata,
-                              Column('place_id', String(60),
-                                     ForeignKey('places.id'),
-                                     nullable=False),
-                              Column('amenity_id', String(60),
-                                     ForeignKey('amenities.id'),
-                                     nullable=False))
+    """Place class"""
+    __tablename__ = 'places'
 
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade="all, delete", backref="place")
+    else:
+        @property
+        def reviews(self):
+            """Getter attribute that returns the list of Review instances"""
+            from models import storage
+            from models.review import Review
+            review_list = []
+            reviews_dict = storage.all(Review)
+            for review in reviews_dict.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
