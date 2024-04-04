8874cb7fdcc570d9432f2021b77ffae3cdaaa34b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-04 14:30:56 +0200

    place

--- models/place.py
+++ models/place.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
 """ a class City"""
-from sqlalchemy import Column, String, Integer, Float
-from sqlalchemy import ForeignKey
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class Place(BaseModel):
-    """ a class model of place
-    att:
-        city_id:string
-        user_id:string
-        name:string
-        description: string
-        number_rooms: integer
-        number_bathrooms: integer
-        max_guest: integer
-        price_by_night: integer
-        latitude: float
-        longitude: float
-        amenity_ids: list
-        """
+class Place(BaseModel, Base):
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        metadata = Base.metadata
+        place_amenity = Table("place_amenity", metadata,
+                              Column('place_id', String(60),
+                                     ForeignKey('places.id'),
+                                     nullable=False),
+                              Column('amenity_id', String(60),
+                                     ForeignKey('amenities.id'),
+                                     nullable=False))
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
