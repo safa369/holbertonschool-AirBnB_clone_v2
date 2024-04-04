557f43550c5f11f0f049c3253cc4617d9b4eba42
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-04 13:51:06 +0200

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class FileStorage():
     """a class that serializes instance to JSON file
     and deserializes JSON file to instance
@@ -32,14 +33,22 @@ class FileStorage():
     }
 
     def all(self, cls=None):
-        """Returns the dictionary __objects."""
-        if cls is not None:
-            object = []
-            for key, obj in self.__objects.items():
-                if type(obj).__name__ == cls.__name__:
-                    object[key] = obj       
-                    return object
-        return self.__objects
+        """Returns a dictionary of models currently in the storage"""
+        if not cls:
+            return FileStorage.__objects
+        else:
+            newdict = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    newdict[key] = value
+            return newdict
+
+    def delete(self, obj=None):
+        """Delete obj from __objects"""
+        if obj:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
@@ -54,16 +63,16 @@ class FileStorage():
             with open(FileStorage.__file_path, "w", encoding="utf-8") as jf:
                 json.dump(dict_obj, jf)
 
-
     def reload(self):
         try:
             with open(self.__file_path, "r",
-                          encoding="utf-8") as fo:
+                      encoding="utf-8") as fo:
                 new_dict = json.load(fo)
                 for key, val in new_dict.items():
                     class_name = val['__class__']
                     del val['__class__']
-                    module = __import__('models.' + class_name.lower(), fromlist=[class_name])
+                    module = __import__('models.' + class_name.lower(),
+                                        fromlist=[class_name])
                     class_ = getattr(module, class_name)
                     self.__objects[key] = class_(**val)
         except FileNotFoundError:
