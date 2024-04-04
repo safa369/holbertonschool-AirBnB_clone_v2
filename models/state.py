5163b4008d6876a229e9166c4e36acf04738b3b7
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-04 14:07:15 +0200

    state

--- models/state.py
+++ models/state.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 """This module creates a User class"""
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
+class State(BaseModel, Base):
+    """State class"""
+    __tablename__ = 'states'
 
-class State(BaseModel):
-    """Class for managing state objects"""
-
-    name = ""
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute that returns the list of City instances"""
+            from models import storage
+            from models.city import City
+            city_list = []
+            cities_dict = storage.all(City)
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
