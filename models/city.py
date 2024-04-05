cbd2cceee11fcb4f4a1447a1ab3142cb3589d216
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-05 02:27:54 +0200

    city

--- models/city.py
+++ models/city.py
@@ -1,17 +1,25 @@
-#!/usr/bin/python3
-"""This module creates a User class"""
+#!/usr/bin/python
+""" holds class City"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class City(BaseModel, Base):
-    """ The city class, contains state ID & name """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60),
-                      ForeignKey("states.id", ondelete="CASCADE"),
-                      nullable=False)
-    places = relationship("Place", backref="cities",
-                          cascade="all, delete-orphan",
-                          passive_deletes=True)
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
+
